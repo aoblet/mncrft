3fd26c7f50d8e5f9c9ffa9b58fcfc2a0e184f4f6
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2014-12-22 16:05:06 +0100

    instanced rendering working

--- shaders/multiple_matrix.fs.glsl
+++ shaders/multiple_matrix.fs.glsl
@@ -2,11 +2,12 @@
 
 in vec2 vPosition;
 in vec2 vTexture;
+in vec3 vColor;
 
 uniform vec3 uColor;
 
 out vec3 fFragColor;
 
 void main() {
-    fFragColor = uColor;
+    fFragColor = vec3(1);
 }
--- shaders/multiple_matrix.vs.glsl
+++ shaders/multiple_matrix.vs.glsl
@@ -2,18 +2,22 @@
 
 uniform mat3 uModelMatrix;
 
-layout(location = 1) in vec2 aVertexPosition;
-layout(location = 2) in vec2 aVertexTexture;
+layout(location = 0) in vec2 aVertexPosition;
+layout(location = 1) in vec2 aVertexTexture;
+layout(location = 2) in vec3 trianglePosition;
+
 
 out vec2 vPosition;
 out vec2 vTexture;
+out vec3 vColor;
 
 void main() {
     vPosition = aVertexPosition;
     vTexture = aVertexTexture;
+    vColor = trianglePosition;
 
     mat3 M = uModelMatrix;
 
     vec2 transformed = (M * vec3(aVertexPosition, 1)).xy;
-    gl_Position = vec4(transformed, 0, 1);
+    gl_Position = vec4(aVertexPosition, 0, 1) + vec4(trianglePosition, 0);
 }
