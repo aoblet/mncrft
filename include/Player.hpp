052de19eea025f89103480041443376d305f4a57
Alex <alexdeoiny@gmail.com>
2015-01-02 02:04:28 +0100

    add interaction tools

--- include/Player.hpp
+++ include/Player.hpp
@@ -4,6 +4,8 @@
 #include "glimac/glm.hpp"
 #include "Movement.hpp"
 #include "glimac/SDLWindowManager.hpp"
+#include "GameInteraction.hpp"
+#include "CubeData.hpp"
 
 class Game;
 class Player{
@@ -13,6 +15,8 @@ class Player{
         Movement m_movementModule;
         Game * m_game;
         glm::vec3 m_position;
+        GameInteraction m_gameInteraction;
+        CubeType m_current_CubeType;
 
     public:
         Player(Game * game=nullptr, glm::vec3 const& position=glm::vec3(0,0,0), std::string const& name="Nooby 3000", int life=10);
@@ -24,10 +28,14 @@ class Player{
         Game* game();
         int life() const;
         std::string name() const;
+        GameInteraction& gameInteraction();
+        GameInteraction gameInteraction() const;
+        CubeType currentCubeType() const;
 
         void movePosition(glm::vec3 const& vectorMovement);
         void setPosition(glm::vec3 const& position);
         void setName(std::string const& name);
         void setGame(Game* game);
-        void handleMove(glimac::SDLWindowManager const& events);        
+        void handleMove(glimac::SDLWindowManager const& events);
+        void setCurrentCubeType(CubeType const& type) ;
 };
